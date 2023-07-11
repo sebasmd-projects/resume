c23fa880778596453c48c49a3f71418ad4b50bdf
Sebastian Morales <juan.sebasm2@gmail.com>
2023-07-11 03:46:11 -0500

    Remoción de strong innecesarios

--- src/pages/es/components/resume/qaResume.jsx
+++ src/pages/es/components/resume/qaResume.jsx
@@ -6,16 +6,16 @@ export default function QAResume() {
             </h3>
             <p align="justify">
                 Tengo experiencia experiencia en el campo del control de calidad (QA) y automatización,
-                siendo competente en los lenguajes de <strong>Python</strong>, <strong>Java</strong> y <strong>C#</strong>.
+                siendo competente en los lenguajes de Python, Java y C#.
                 Mi experiencia incluye la utilización de patrones de diseño como lo son <strong>Page Object Model (POM)</strong> y <strong>ScreenPlay</strong>
                 para crear soluciones de automatización de pruebas eficientes y fáciles de mantener.
                 Con un buen ojo para los detalles y fuertes habilidades analíticas, destaco en la depuración e identificación de errores de software.
             </p>
             <p align="justify">
                 He trabajado con varios marcos y herramientas de automatización,
-                incluyendo <strong>Cypress</strong>, <strong>Selenium</strong>, <strong>Cucumber</strong>, <strong>Gherkin</strong>, <strong>JUnit</strong>,
-                <strong>NUnit</strong>, <strong>Pytest</strong>, <strong>Serenity</strong> y más.
-                Además, tengo experiencia en la automatización de dispositivos móviles utilizando <strong>Appium</strong> y <strong>BrowserStack</strong>.
+                incluyendo <strong>Cypress</strong>, <strong>Selenium</strong>, Cucumber, <strong>Gherkin</strong>, JUnit,
+                NUnit, Pytest, Serenity y más.
+                Además, tengo experiencia en la automatización de dispositivos móviles utilizando Appium y BrowserStack.
                 Esta experiencia diversa me ha proporcionado una comprensión integral de las limitaciones y oportunidades disponibles para ofrecer las mejores soluciones posibles.
             </p>
             <p align="justify">
